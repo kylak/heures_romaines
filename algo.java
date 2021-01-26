c93ba4bbf7e34edc842940e5cc86e57bac284ab5
kylak <gustavberloty2@gmail.com>
2021-01-26 13:41:13 +0100

    algo.java réorganisé
    Je pense que la pile d'exécution devrait maintenant pouvoir se lire assez facilement en remontant du bas du fichier jusqu'à son début.

--- algo.java
+++ algo.java
@@ -3,40 +3,8 @@ public class algo {
 
 	// args[0]: lever du soleil (en min.)
 	// args[1]: durée du jour (en min.)
-
-	public static void main 
-	(String[] args) {
-		int dr = n_ntr(args[1]);
-		float lv = n_dcml(args[0]);
-		float mnts =  dr / 12.F;
-		int[][][] h = id_hr(mnts, lv);
-		int[]dr_hr = dcp_mnts(mnts);
-		ffch_dr_hr(dr_hr);
-		ffch(h);
-	}
 	
-	// [convertit] en entier
-	public static int n_ntr
-	(String prm) {
-		return Integer.parseInt(prm);
-	}
-	
-	// [convertit] en décimal
-	public static float n_dcml
-	(String prm) {
-		return Float.parseFloat(prm);
-	}
-	
-	// [afficher 1] heure romaine
-	public static void ffch_dr_hr
-	(int[] hr) {
-		System.out.println();
-		System.out.println("1 heure "
-		+ "temporaire: " + hr[0] + "h"
-		+ hr[1] + "m" + hr[2] + "s.");
-	}
-	
-	// [afficher des ] heures
+	// [afficher une] heure
 	public static String hr
 	(int[][][] h, int i, int prt) {
 		return h[i][prt][0] + "h" 
@@ -44,7 +12,7 @@ public class algo {
 		h[i][prt][2] + "s";
 	}
 	
-	// [afficher des ] phrases
+	// [afficher la] phrase
 	public static String phrs
 	(int i, int[][][] h, int prt) {
 		return " de la " + i + 
@@ -61,6 +29,49 @@ public class algo {
 			phrs(i, h, 0) + "\nMoitié" + 
 			phrs(i, h, 1) + "\n");
 		}
+	}
+	
+	// [afficher 1] heure romaine
+	public static void ffch_dr_hr
+	(int[] hr) {
+		System.out.println();
+		System.out.println("1 heure "
+		+ "temporaire: " + hr[0] + "h"
+		+ hr[1] + "m" + hr[2] + "s.");
+	}
+	
+	// mettre-à-jour
+	public static int[] mttr_jr
+	(int[] h, int i) {
+		h[i + 1] = 0;
+		h[i]++;
+		return h;
+	}
+	
+	// verifier
+	public static int[] vrfr
+	(int[] h) {
+		if(h[2] == 60) {
+			mttr_jr(h, 1);
+			if(h[1] == 60)
+				mttr_jr(h, 0);
+		}
+		return h;
+	}
+	
+	// découpe des minutes 
+	// [0] : hr, [1] : mnt, 
+	// [2] : scnd
+	public static int[] dcp_mnts
+	(float mnts) {
+		float hd = mnts / 60.F;
+		int[] h = new int[3];
+		h[0] = (int) hd;
+		float hd2 = (hd - h[0]) * 60.F;
+		h[1] = (int) hd2;
+		float hd3 = (hd2 - h[1]) * 60.F;
+		h[2] = Math.round(hd3);
+		return vrfr(h);
 	}
 	
 	public static int[][][] crrH
@@ -83,39 +94,27 @@ public class algo {
 		return h;
 	}
 	
-	// mettre-à-jour
-	public static int[] mttr_jr
-	(int[] h, int i) {
-		h[i + 1] = 0;
-		h[i]++;
-		return h;
+	// [convertit] en décimal
+	public static float n_dcml
+	(String prm) {
+		return Float.parseFloat(prm);
 	}
 	
-	// verifier
-	public static int[] vrfr
-	(int[] h) {
-		if(h[2] == 60) {
-			mttr_jr(h, 1);
-			if(h[1] == 60) {
-				mttr_jr(h, 0);
-			}
-		}
-		return h;
+	// [convertit] en entier
+	public static int n_ntr
+	(String prm) {
+		return Integer.parseInt(prm);
 	}
-	
-	// découpe des minutes 
-	// [0] : hr, [1] : mnt, 
-	// [2] : scnd
-	public static int[] dcp_mnts
-	(float mnts) {
-		float hd = mnts / 60.F;
-		int[] h = new int[3];
-		h[0] = (int) hd;
-		float hd2 = (hd - h[0]) * 60.F;
-		h[1] = (int) hd2;
-		float hd3 = (hd2 - h[1]) * 60.F;
-		h[2] = Math.round(hd3);
-		return vrfr(h);
+
+	public static void main 
+	(String[] args) {
+		int dr = n_ntr(args[1]);
+		float lv = n_dcml(args[0]);
+		float mnts =  dr / 12.F;
+		int[][][] h = id_hr(mnts, lv);
+		int[]dr_hr = dcp_mnts(mnts);
+		ffch_dr_hr(dr_hr);
+		ffch(h);
 	}
 }
 
