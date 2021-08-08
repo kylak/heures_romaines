e21c52a48e9f34586941a43a94b08f6c78852328
kylak <gustavberloty2@gmail.com>
2021-08-08 10:23:24 +0200

    ajout de la fonctionnalité openMap

--- position.js
+++ position.js
@@ -1,21 +1,31 @@
 function geoFindMe() {
 
   const status = document.querySelector('#status');
-  status.textContent = '';
+  const lat = document.querySelector('#lat');
+  const lon = document.querySelector('#lon');
+
+  lat.href = '';
+  lat.textContent = '';
+  lon.href = '';
+  lon.textContent = '';
 
   function success(position) {
     const latitude  = position.coords.latitude;
     const longitude = position.coords.longitude;
 
-    status.innerHTML = `Latitude: ${latitude} °<br>Longitude: ${longitude} °`;
+    status.textContent = '';
+    lat.href = `https://www.openstreetmap.org/#map=18/${latitude}/${longitude}`;
+    lon.href = `https://www.openstreetmap.org/#map=18/${latitude}/${longitude}`;
+    lat.textContent = `Latitude: ${latitude} °`;
+    lon.textContent = `Longitude: ${longitude} °`;
   }
 
   function error() {
-    status.textContent = 'Impossible de retrouver votre position.';
+    status.textContent = 'Impossible de retrouver votre position';
   }
 
   if(!navigator.geolocation) {
-    status.textContent = 'La géolocalisation n\'est pas prise en charge par votre navigateur.';
+    status.textContent = 'La géolocalisation n\'est pas prise en charge par votre navigateur';
   } else {
     status.textContent = 'Localisation en cours..';
     navigator.geolocation.getCurrentPosition(success, error);
